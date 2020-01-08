0fbe0b497ec539513623919d7f3d122d726466e7
Brick409 <lrxhappy2008@163.com>
2020-01-08 23:16:52 +0800

    添加m*n矩阵， 动态寻找最小路径的方法

--- Dynamic-programming/Dynamic-programming/Dynamic-programming.cpp
+++ Dynamic-programming/Dynamic-programming/Dynamic-programming.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <time.h>
 #include <malloc.h>
+#include <math.h>
 
 using namespace std;
 #define MAX 101
@@ -54,9 +55,70 @@ public:
     /* LeetCode64:给出一个m*n的矩阵，其中每一个格子包含一个非负整数。寻找一条从左上角到右下脚的路径，
        是的沿路的数字和最小。（每一步只能右移或者下移）
     */
-    int matrixMinPathSum(int n)
+    /*
+    (动态规划)：时间复杂为o(n*m)和空间复杂度为o(n*m)。
+    典型的动态规划问题，假设当前已经开始计算s[i][j]，
+    那么s[i][j]只可能从s[i-1][j]+grid[i][j]或者s[i][j-1]+grid[i][j]计算得到，
+    也就是s[i][j] = min(s[i-1][j],s[i][j-1])+grid[i][j]。
+    我们需要一个o(n*m)额外空间保存已经计算的s[i][j]的值，我们只需要访问一遍数组即可。
+    因此时间复杂度为o(n*m)，空间复杂度为o(n*m)。
+    我们需要特殊处理矩阵中第一行和第一列。
+    因为第一行没有s[i-1][j]元素，只有s[i][j-1]元素。
+    第一列没有s[i][j-1]元素，只有s[i-1][j]元素。
+    */
+    int matrixMinPathSum(vector<vector<int>>& grid)
     {
-        return 0;
+        int minSum = 0;
+        //当grid为空时，返回0
+        if (grid.empty())
+        {
+            return 0;
+        }
+
+        //获得grid行数
+        int row = grid.size();
+        //获得grid的列数
+        int col = grid[0].size();
+        //定义一个二维vector，用于存储对应路径和
+        vector<vector<int>> preSum(row);
+
+        //创建该vector,全部为0
+        for (int i = 0; i < row; i++) {
+            preSum[i].resize(col);
+        }
+
+        preSum[0][0] = grid[0][0];
+
+        //初始化第一行数据
+        for (int i = 1; i < col;i++)
+        {
+            preSum[0][i] = preSum[0][i-1] + grid[0][i];
+        }
+        //初始化第一列数据
+        for (int i = 1; i < row; i++)
+        {
+            preSum[i][0] = preSum[i-1][0] + grid[i][0];
+        }
+
+        //计算a[i][j]对应的preSum的值
+        for (int i = 1; i < row;i++)
+        {
+            for (int j = 1; j < col;j++)
+            {
+                if (preSum[i - 1][j]<preSum[i][j - 1])
+                {
+                    minSum = preSum[i - 1][j];
+                }
+                else
+                {
+                    minSum = preSum[i][j - 1];
+                }
+                //a[i][j]只能通过上一个结点或前一个结点走到
+                preSum[i][j] = minSum + grid[i][j];
+            }  
+        }
+
+        return preSum[row-1][col-1];
     }
 };
 
@@ -65,7 +127,7 @@ public:
 int _tmain(int argc, _TCHAR* argv[])
 {
     Solution solution;
-
+#if 0
     int i = 0, j = 0;
     //输入三角型对应的行数n
     cout << "输入三角形的行数：" << endl;
@@ -83,6 +145,14 @@ int _tmain(int argc, _TCHAR* argv[])
     int sumMin = solution.triangleMinSum(0, 0);
 
     cout << sumMin << endl;
+#endif
+
+    vector<vector<int> >num =
+    { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+
+    int sumMinWay = solution.matrixMinPathSum(num);
+
+    cout << sumMinWay << endl;
 
     system("pause");
     return 0;
